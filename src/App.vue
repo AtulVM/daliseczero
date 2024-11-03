48444f59416eccbb082a68bd9b2781b41ad99798
Atul V Menon <atulmenon39@gmail.com>
2024-11-03 21:31:27 +0530

    tailwind configured

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100">
+    <header class="bg-blue-500 text-white p-4">
+      <h1 class="text-center text-2xl">Dalisec</h1>
+    </header>
+    <main class="p-4">
+      <center>
+        <div>This is the main content area</div>
+      </center>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
